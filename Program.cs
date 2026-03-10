08c7c6111e664102d06715ffa55567b707913b71
alexcolpin <alex.colpin@edouardgand.fr>
2026-03-10 08:05:00 +0100

    updat programme.cs

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Car car3 = new Car("Toyota", "Corolla");
+        public Car(string brand, string model)
+        Car car4 = new Car("Toyota", "Corolla", 2020);
     }
 }
